46331df9d225ed21db694eccf52965853b6a4ab1
Pablo Brasero <pablo@new-bamboo.co.uk>
2013-05-31 16:34:41 +0100

    Pristine Rails 3.2.13 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnionjsExample::Application.config.secret_token = '89841746600b57bbafb1b93f8315a10e3a39b1f6645bc36a4202543fef3e3201fa4e1fbd6c930329907259ea33399c4e971f1977338bff2da08308407840cf3f'
